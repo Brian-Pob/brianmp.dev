4cec0f38f04193b3f3e4f30958043edd38da6875
Brian-Pob <brianpoblete77777@gmail.com>
2024-02-27 04:34:21 -0500

    try to remove pnpm and use hybrid rendering on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://brianmp.dev',
   integrations: [mdx(), sitemap()],
+  output: "hybrid",
+  adapter: vercel()
 });
