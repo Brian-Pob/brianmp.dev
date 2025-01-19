0b375feeae180218b4266e29649b9872c8f70e72
Brian-Pob <brianmp.dev@gmail.com>
2025-01-19 15:13:44 -0800

    update packages and update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://brianmp.dev',
-  integrations: [mdx(), sitemap()],
-  output: "hybrid",
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  })
+	site: "https://brianmp.dev",
+	integrations: [mdx(), sitemap()],
+	output: "static",
+	adapter: vercel({
+		webAnalytics: { enabled: true },
+	}),
 });
