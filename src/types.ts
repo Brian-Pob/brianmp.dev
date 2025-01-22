83a4351f5dd2d467d1636fde3bf7fda8e30b354b
Brian-Pob <brianmp.dev@gmail.com>
2025-01-21 16:17:12 -0800

    update viewtransitions and add comments to types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,30 @@
+/**
+* Used for adding extra <meta> tags for Open Graph. These tags should
+* automatically override existing meta tags of the same name when read by the
+* browser.
+*
+* @property {string} property - The property of the tag.
+* @property {string} content - The content that gets displayed.
+* @property {boolean} isName - Should the tag use 'name' instead of 'property'.
+
+* @example
+* const extra_og: OGPropsExtra = {
+*   property: 'og:image',
+*   content: 'https://example.com/example.jpg',
+*   isName: false
+* }
+* // Output
+* <meta property="og:image" content="https://example.com/example.jpg" />
+*
+* @example
+* const extra_og: OGPropsExtra = {
+*   property: 'og:image:alt',
+*   content: 'Example Image',
+*   isName: true
+* }
+* // Output
+* <meta name="og:image:alt" content="Example Image" />
+*/
 type OGPropsExtra = {
   property: string;
   content: string;
