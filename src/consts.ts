6cc6a0563c02c0d2d51af9ec5c9b0aaaf45a7ddd
Brian-Pob <brianpoblete77777@gmail.com>
2025-03-20 14:20:12 -0700

    update astro to latest. start style rewrite

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Brian Poblete';
+export const SITE_TITLE = "Brian Poblete";
 export const SITE_DESCRIPTION =
-  "Hi there! I'm a Tallahasse based UX Designer and Software Engineer. I'm passionately building and desiging for the web. Come get to know me!";
+	"Hi there! I'm a Seattle-based Frontend Engineer and budding UX Designer. I'm passionately building and desiging a better web for all. Come get to know me!";
